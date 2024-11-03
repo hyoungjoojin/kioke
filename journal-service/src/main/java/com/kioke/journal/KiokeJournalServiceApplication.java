35ecaa1077a28a8246afbf5e18268167ed5d8f9c
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2024-11-03 13:49:52 +0000

    feat: add initial journal service application

---
+++ journal-service/src/main/java/com/kioke/journal/KiokeJournalServiceApplication.java
@@ -0,0 +1,12 @@
+package com.kioke.journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiokeJournalServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KiokeJournalServiceApplication.class, args);
+  }
+}
