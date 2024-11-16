0e1857aa6e8cb58c2f4e865a5cb0f6917c483d9a
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2024-11-16 02:33:59 +0000

    feat: add initial user service application

---
+++ user-service/src/test/java/com/kioke/user/KiokeUserServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.kioke.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiokeUserServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
