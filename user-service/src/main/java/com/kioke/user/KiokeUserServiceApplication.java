1aadccee855fc066dbe1f52670a60b794a484889
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2024-11-16 11:42:37 +0900

    feat: add initial user service application (#20)

---
+++ user-service/src/main/java/com/kioke/user/KiokeUserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.kioke.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiokeUserServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KiokeUserServiceApplication.class, args);
+  }
+}
