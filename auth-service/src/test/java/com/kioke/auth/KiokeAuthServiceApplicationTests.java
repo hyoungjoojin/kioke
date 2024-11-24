d3e98c4eac410cb801fab7336a678784fb06ecde
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2024-11-24 10:01:54 +0000

    feat: add initial auth service application

---
+++ auth-service/src/test/java/com/kioke/auth/KiokeAuthServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.kioke.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiokeAuthServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
