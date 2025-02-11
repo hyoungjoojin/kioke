40157bb5c68864256a52b578844b6a948ece128b
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-02-11 09:45:14 +0000

    feat: add base layout for home page in web client

--- web-client/src/app/(home)/page.tsx
+++ web-client/src/app/(home)/page.tsx
@@ -1,6 +1,14 @@
 import { auth } from "@/lib/auth";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { redirect } from "next/navigation";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default async function Home() {
   const session = await auth();
@@ -9,6 +17,22 @@ export default async function Home() {
   if (!user) {
     redirect("/auth/login");
   }
+
+  // Test Data
+  const journals = [
+    {
+      title: "Journal #1",
+      description: "Journal #1 Description",
+    },
+    {
+      title: "Journal #2",
+      description: "Journal #2 Description",
+    },
+    {
+      title: "Journal #3",
+      description: "Journal #3 Description",
+    },
+  ];
 
   return (
     <>
@@ -22,6 +46,31 @@ export default async function Home() {
           <span></span>
         </div>
       </header>
+      <main>
+        <div className="w-full flex flex-col justify-center items-center">
+          <h1 className="text-3xl my-10">Shelf Name</h1>
+          <div className="lg:w-1/2 w-4/5">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Title</TableHead>
+                  <TableHead>Description</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {journals.map((journal, index) => {
+                  return (
+                    <TableRow key={index}>
+                      <TableCell>{journal.title}</TableCell>
+                      <TableCell>{journal.description}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
