740f6c84c9dcd3701b832b72874a7bfbf814407d
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-02-17 13:51:25 +0000

    feat: add button for creating new journals

---
+++ web-client/src/app/@modal/journal/new/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import Modal from "@/components/ui/modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import { useBoundStore } from "@/components/providers/StoreProvider";
+import { createJournal } from "@/app/api/journal";
+import { useRouter } from "next/navigation";
+
+const CreateJournalFormSchema = z.object({
+  title: z.string().nonempty(),
+});
+
+export default function CreateJournalModal() {
+  const router = useRouter();
+
+  const selectedShelf = useBoundStore((state) =>
+    state.actions.getSelectedShelf(),
+  );
+
+  const createJournalForm = useForm<z.infer<typeof CreateJournalFormSchema>>({
+    resolver: zodResolver(CreateJournalFormSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const formSubmitHandler = async (
+    values: z.infer<typeof CreateJournalFormSchema>,
+  ) => {
+    const { title } = values;
+
+    if (selectedShelf) {
+      await createJournal(title, selectedShelf.id);
+    }
+
+    router.back();
+  };
+
+  return (
+    <Modal title="Add journal" variant="sm">
+      <Form {...createJournalForm}>
+        <form onSubmit={createJournalForm.handleSubmit(formSubmitHandler)}>
+          <FormField
+            name="title"
+            control={createJournalForm.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <div className="flex absolute bottom-10 right-10 justify-center">
+            <Button type="submit" className="w-full">
+              Add
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </Modal>
+  );
+}
