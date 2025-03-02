b580c93affbd1cda2718f7cb1d64d98ebdebe65a
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-03-02 00:56:43 +0000

    feat: add dialog for deleting journals

--- web-client/src/components/pages/home/JournalList.tsx
+++ web-client/src/components/pages/home/JournalList.tsx
@@ -18,6 +18,87 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { DialogFooter, DialogHeader } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { JSX, useState } from "react";
+
+interface JournalListItemMenuProps {
+  title: string;
+}
+
+const JournalListItemMenu = ({ title }: JournalListItemMenuProps) => {
+  enum JournalListItemMenuModal {
+    DELETE_JOURNAL = "delete",
+  }
+
+  const [modalState, setModalState] = useState<{
+    open: boolean;
+    modal: JournalListItemMenuModal | null;
+  }>({
+    open: false,
+    modal: null,
+  });
+
+  const openModal = (modal: JournalListItemMenuModal) => {
+    setModalState(() => ({ open: true, modal }));
+  };
+
+  const closeModal = () => {
+    setModalState(() => ({ open: false, modal: null }));
+  };
+
+  const modals: { [key in JournalListItemMenuModal]: JSX.Element } = {
+    [JournalListItemMenuModal.DELETE_JOURNAL]: (
+      <DialogContent onEscapeKeyDown={closeModal} hideCloseButton>
+        <DialogHeader>
+          <DialogTitle>Delete journal?</DialogTitle>
+          <DialogDescription>
+            Journal <span className="italic">{title}</span> and its pages will
+            be permanently deleted.
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <Button onClick={closeModal}>Cancel</Button>
+          <Button type="submit" variant="destructive">
+            Delete
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    ),
+  };
+
+  return (
+    <Dialog open={modalState.open} onOpenChange={closeModal}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div>
+            <Ellipsis size={15} />
+          </div>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align="center">
+          <DropdownMenuItem
+            onSelect={() => openModal(JournalListItemMenuModal.DELETE_JOURNAL)}
+          >
+            <div className="flex items-center text-red-500">
+              <Trash2 size={16} className="mx-1" />
+              Delete Journal
+            </div>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {modalState.open && modalState.modal && modals[modalState.modal]}
+    </Dialog>
+  );
+};
 
 interface JournalListItemProps {
   id: string;
@@ -47,21 +128,7 @@ const JournalListItem = ({ id, title }: JournalListItemProps) => {
           e.stopPropagation();
         }}
       >
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <div>
-              <Ellipsis size={15} />
-            </div>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent loop align="center">
-            <DropdownMenuItem>
-              <div className="flex items-center text-red-500">
-                <Trash2 size={16} className="mx-1" />
-                Delete Journal
-              </div>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <JournalListItemMenu title={title} />
       </TableCell>
     </TableRow>
   );
@@ -75,8 +142,8 @@ export default function JournalList() {
     <Table>
       <TableHeader>
         <TableRow>
-          <TableHead>Title</TableHead>
-          <TableHead>Description</TableHead>
+          <TableHead className="select-none">Title</TableHead>
+          <TableHead className="select-none">Description</TableHead>
           <TableHead></TableHead>
         </TableRow>
       </TableHeader>
