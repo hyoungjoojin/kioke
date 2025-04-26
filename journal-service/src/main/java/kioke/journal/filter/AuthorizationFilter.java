2d195580a4e5fe8374afa98c3bba14b5a393f0ba
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-04-26 06:51:55 +0000

    feat: add codegen tools to web client

--- journal-service/src/main/java/kioke/journal/filter/AuthorizationFilter.java
+++ journal-service/src/main/java/kioke/journal/filter/AuthorizationFilter.java
@@ -1,9 +1,13 @@
 package kioke.journal.filter;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import kioke.commons.filter.AbstractAuthorizationFilter;
 import kioke.commons.service.AbstractAuthService;
 import kioke.journal.service.AuthService;
 import kioke.journal.service.UserService;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
@@ -13,9 +17,18 @@ public class AuthorizationFilter extends AbstractAuthorizationFilter {
   private final UserService userService;
   private final AuthService authService;
 
+  private final List<HttpRequest> whitelist =
+      Collections.unmodifiableList(
+          Arrays.asList(new HttpRequest(HttpMethod.GET, "/v3/api-docs/journal")));
+
   public AuthorizationFilter(UserService userService, AuthService authService) {
     this.userService = userService;
     this.authService = authService;
+  }
+
+  @Override
+  public List<HttpRequest> getWhitelist() {
+    return whitelist;
   }
 
   @Override
