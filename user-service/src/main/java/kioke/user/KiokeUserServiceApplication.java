3a7866e5f8e9b568acb2664a8b42831db53bfe1e
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-05-06 04:58:36 +0000

    test
    testing if the formatters are working properly

--- user-service/src/main/java/kioke/user/KiokeUserServiceApplication.java
+++ user-service/src/main/java/kioke/user/KiokeUserServiceApplication.java
@@ -4,9 +4,5 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KiokeUserServiceApplication {
-
-  public static void main(String[] args) {
-    SpringApplication.run(KiokeUserServiceApplication.class, args);
-  }
+public class KiokeUserServiceApplication { public static void main(String[] args) { SpringApplication.run(KiokeUserServiceApplication.class, args); }
 }
