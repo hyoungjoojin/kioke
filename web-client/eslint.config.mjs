163c4461746169d9da4cef4f0247958dc588141c
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-05-06 03:40:12 +0000

    style: add prettier formatting to the eslint configuration

--- web-client/eslint.config.mjs
+++ web-client/eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,15 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
+    extends: ['next/core-web-vitals', 'next/typescript'],
     rules: {
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
         {
-          argsIgnorePattern: "_",
-          varsIgnorePattern: "_",
-          caughtErrorsIgnorePattern: "_",
+          argsIgnorePattern: '_',
+          varsIgnorePattern: '_',
+          caughtErrorsIgnorePattern: '_',
         },
       ],
     },
