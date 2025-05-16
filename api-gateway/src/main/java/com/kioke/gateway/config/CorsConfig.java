fb6b7c7d63564b7dfcb461c4ef1153e21b648afa
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-05-16 09:37:03 +0900

    chore: add github actions workflow (#135)
    Added GitHub workflow files for CI/CD workflow.
    
    ## Implementation Details
    ### Key Changes
    - Added a GitHub actions file for CI workflow.

--- api-gateway/src/main/java/com/kioke/gateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/kioke/gateway/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.kioke.gateway.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
--- user-service/src/main/java/kioke/user/KiokeUserServiceApplication.java
+++ user-service/src/main/java/kioke/user/KiokeUserServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KiokeUserServiceApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(KiokeUserServiceApplication.class, args);
   }
