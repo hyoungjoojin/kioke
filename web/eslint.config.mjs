c8e7ed4ec47071a7252a3800baf1c6a7da1bc2e8
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-08-12 18:51:20 +0900

    chore: set up initial web client

---
+++ web/eslint.config.mjs
@@ -0,0 +1,38 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...pluginQuery.configs['flat/recommended'],
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '_',
+          varsIgnorePattern: '_',
+          caughtErrorsIgnorePattern: '_',
+        },
+      ],
+    },
+  }),
+  {
+    rules: {
+      'no-console': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+    },
+  },
+];
+
+export default eslintConfig;
