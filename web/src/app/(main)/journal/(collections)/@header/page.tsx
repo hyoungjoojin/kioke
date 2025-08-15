ef7a0fc59509265e6069a70689fe084e34ebbcc0
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-08-15 16:41:50 +0900

    feat: add profile dropdown in web client

--- web/src/app/(main)/journal/(collections)/@header/page.tsx
+++ web/src/app/(main)/journal/(collections)/@header/page.tsx
@@ -1,3 +1,4 @@
+import ProfileAvatar from '@/components/feature/profile/ProfileAvatar';
 import { Routes } from '@/constant/routes';
 import { getTranslations } from 'next-intl/server';
 import Link from 'next/link';
@@ -6,11 +7,19 @@ export default async function JournalCollectionsHeader() {
   const t = await getTranslations();
 
   return (
-    <div className='flex gap-2'>
-      <span className='italic mr-4'>kioke.</span>
-      <span className='hover:underline'>
-        <Link href={Routes.HOME}>{t('header.links.home')}</Link>
-      </span>
+    <div className='flex justify-between items-center'>
+      <div className='flex gap-2'>
+        <span className='italic mr-4'>kioke.</span>
+        <span className='hover:underline'>
+          <Link href={Routes.HOME}>{t('header.links.home')}</Link>
+        </span>
+      </div>
+
+      <div>
+        <span>
+          <ProfileAvatar />
+        </span>
+      </div>
     </div>
   );
 }
---
+++ web/src/components/feature/profile/ProfileAvatar.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { ModalType } from '@/components/modal/Modal';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import Icon, { IconName } from '@/components/ui/icon';
+import { useModalActions } from '@/store/modal';
+import { useTranslations } from 'next-intl';
+
+export default function ProfileAvatar() {
+  const t = useTranslations();
+  const { openModal } = useModalActions();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar>
+          <AvatarImage src={''} />
+          <AvatarFallback>
+            <Icon name={IconName.USER} fill='none' />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent side='bottom' align='end'>
+        <DropdownMenuItem
+          icon={IconName.SETTINGS}
+          onClick={() => {
+            openModal(ModalType.SETTINGS);
+          }}
+        >
+          {t('header.profile.settings')}
+        </DropdownMenuItem>
+        <DropdownMenuItem icon={IconName.SIGN_OUT}>
+          {t('header.profile.sign-out')}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
