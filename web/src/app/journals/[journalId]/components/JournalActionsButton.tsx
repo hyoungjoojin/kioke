f1e100c37b03aa3368bdf590cff01170c3d74320
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-09-23 18:33:18 +0900

    feat: add ui for updating journal cover images

--- web/src/app/journals/[journalId]/components/JournalActionsButton.tsx
+++ web/src/app/journals/[journalId]/components/JournalActionsButton.tsx
@@ -6,6 +6,14 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { IconName } from '@/components/ui/icon';
+import { Input } from '@/components/ui/input';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { useClickAway } from '@uidotdev/usehooks';
+import { useState } from 'react';
 
 interface JournalActionsButtonProps {
   journalId: string;
@@ -21,10 +29,50 @@ export default function JournalActionsButton({
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align='end' sideOffset={10}>
+        <UpdateCoverButton />
         <DropdownMenuItem icon={IconName.TRASH}>
           Delete Journal
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
+  );
+}
+
+function UpdateCoverButton() {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const ref = useClickAway<HTMLDivElement>(() => {
+    setIsPopoverOpen(false);
+  });
+
+  return (
+    <>
+      <DropdownMenuItem
+        icon={IconName.IMAGE}
+        onSelect={(e) => {
+          e.preventDefault();
+          setIsPopoverOpen(true);
+        }}
+      >
+        Update Cover
+      </DropdownMenuItem>
+
+      <Popover open={isPopoverOpen}>
+        <PopoverTrigger
+          asChild
+          onClick={() => {
+            setIsPopoverOpen(true);
+          }}
+        >
+          <span />
+        </PopoverTrigger>
+        <PopoverContent ref={ref} side='right' className='mx-1'>
+          <span>Update Journal Cover</span>
+          <span></span>
+
+          <Input type='file' />
+        </PopoverContent>
+      </Popover>
+    </>
   );
 }
